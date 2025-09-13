261a369941093d37d8e1a4dcb0a377ce19f75766
irinagvozdeva <121356350+irinagvozdeva@users.noreply.github.com>
2025-09-13 23:56:02 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,14 +5,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // Объявляем объект класса Scanner для ввода данных
         Scanner in = new Scanner(System.in);
+        // Считывание пяти целых чисел из консоли
         int X = in.nextInt();
         int A = in.nextInt();
         int B = in.nextInt();
         int C = in.nextInt();
         int D = in.nextInt();
+        // Создание переменной-счетчика
         int k = 0;
-
+        // Проверка, проходит ли шарик отверстия. Если не проходит - вывод ответа,
+        // если проходит - увелечение k на один и переход к следующему отверстию.
         if (X>=A) {
             System.out.println(k);
         }
